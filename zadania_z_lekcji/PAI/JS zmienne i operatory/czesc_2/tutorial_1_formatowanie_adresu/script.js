8e6f1ca9e2a6bf7389dbed8d3af6511ac750eb03
4eilukasz <szkola.zsti123@gmail.com>
2026-09-23 15:44:51 +0200

    Add files via upload

--- zadania_z_lekcji/PAI/JS zmienne i operatory/czesc_2/tutorial_1_formatowanie_adresu/script.js
+++ zadania_z_lekcji/PAI/JS zmienne i operatory/czesc_2/tutorial_1_formatowanie_adresu/script.js
@@ -1,1 +1,6 @@
 // Pobierz dane i wyświetl adres
+let ulica = prompt("Podaj ulicę");
+let numer_domu = Number(prompt("Podaj numer domu"));
+let kod_pocztowy = prompt("Podaj kod pocztowy (XX-XXX)");
+let miasto = prompt("Podaj miasto");
+document.write(ulica + " " + numer_domu + "</br>" + kod_pocztowy + " " + miasto);
--- zadania_z_lekcji/PAI/JS zmienne i operatory/czesc_2/tutorial_6_magazynek_amunicji/script.js
+++ zadania_z_lekcji/PAI/JS zmienne i operatory/czesc_2/tutorial_6_magazynek_amunicji/script.js
@@ -1,2 +1,9 @@
 // Użyj --, -= oraz +=
 let amunicja = 20;
+document.write("Start: " + amunicja + " pocisków </br>");
+amunicja--;
+document.write(amunicja + "</br>");
+amunicja-=3;
+document.write(amunicja + "</br>");
+amunicja+=10;
+document.write(amunicja);
